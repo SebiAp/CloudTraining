2aa85c1c500fc4e5dc9c49e4503b996d6ee87216
Sebastian Apascaritei <sebastian.apascaritei@centric.eu>
2022-01-27 11:59:08 +0200

    new version

--- ExampleProject/Centric.eCommerce.Product.Api/DB/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Centric.eCommerce.Product.Api.DB;
-
-public class Product
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public int Inventory { get; set; }
-}
